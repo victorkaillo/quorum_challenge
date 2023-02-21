c3d56b626637d653a95eaa9d08880868f0a95685
Victor Kaillo <victorkaillo@hotmail.com>
2023-02-21 13:30:12 -0300

    make files

--- src/data_extract_service.py
+++ src/data_extract_service.py
@@ -17,35 +17,45 @@ class DataExtract:
                 f"File {file_name} not found: check if the path to file is quorum_challenge/raw_data/{file_name}.csv"
             )
 
-    def create_legislators_support_oppose_count(self):
-        legislators_support_oppose_count_dict = dict()
+    def make_files(self):
+        self.legislators_support_oppose_count_dict = dict()
+        self.bills_dict = dict()
         for _, legislator in self.vote_results.iterrows():
-            if not legislators_support_oppose_count_dict.get(
+            if not self.legislators_support_oppose_count_dict.get(
                 legislator["legislator_id"]
             ):
-                legislators_support_oppose_count_dict[legislator["legislator_id"]] = {
-                    "id": legislator["legislator_id"],
-                    "name": self.legislators.loc[
-                        self.legislators.id == legislator["legislator_id"], "name"
-                    ].values[0],
-                    "num_supported_bills": 0,
-                    "num_opposed_bills": 0,
-                }
-
+                self.create_legislators_support_oppose_count_dict(legislator)
+            if not self.bills_dict.get(legislator["vote_id"]):
+                self.create_bills_dict(legislator)
             if legislator["vote_type"] == 1:
-                legislators_support_oppose_count_dict[legislator["legislator_id"]][
+                self.legislators_support_oppose_count_dict[legislator["legislator_id"]][
                     "num_supported_bills"
                 ] += 1
+                self.bills_dict[legislator["vote_id"]]["supporter_count"] += 1
             elif legislator["vote_type"] == 2:
-                legislators_support_oppose_count_dict[legislator["legislator_id"]][
+                self.legislators_support_oppose_count_dict[legislator["legislator_id"]][
                     "num_opposed_bills"
                 ] += 1
+                self.bills_dict[legislator["vote_id"]]["opposer_count"] += 1
+
         legislators_support_oppose_count = pd.DataFrame(
-            legislators_support_oppose_count_dict
+            self.legislators_support_oppose_count_dict
         ).T
         legislators_support_oppose_count.to_csv(
             "legislators_support_oppose_count.csv", index=False
         )
+        bills = pd.DataFrame(self.bills_dict).T
+        bills.to_csv("bills.csv", index=False)
 
-    def create_bills(self):
+    def create_legislators_support_oppose_count_dict(self, legislator):
+        self.legislators_support_oppose_count_dict[legislator["legislator_id"]] = {
+            "id": legislator["legislator_id"],
+            "name": self.legislators.loc[
+                self.legislators.id == legislator["legislator_id"], "name"
+            ].values[0],
+            "num_supported_bills": 0,
+            "num_opposed_bills": 0,
+        }
+
+    def create_bills_dict(self):
         pass
